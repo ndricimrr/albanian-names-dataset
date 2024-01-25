ae5e535d1141c033659272f41f530692f6cbf61a
ndricimrr <ndricimrr@gmail.com>
2024-01-25 23:27:36 +0100

    Test first script

--- scripts/merge_names.js
+++ scripts/merge_names.js
@@ -1,3 +1,47 @@
 "use strict";
 
-console.log();
+const fs = require("fs");
+
+const all_names_path = "all_names.json";
+const male_names_path = "../female.json";
+const female_names_path = "../male.json";
+
+const redColor = "\x1b[31m";
+const resetColor = "\x1b[0m";
+const greenColor = "\x1b[32m";
+const yellowColor = "\x1b[33m";
+
+try {
+  const male_names_list = require(`./${male_names_path}`);
+  const female_names_list = require(`./${female_names_path}`);
+
+  mergeLists(male_names_list, female_names_list, all_names_path);
+} catch (error) {
+  console.log(
+    redColor + "Error reading JSON file:\n" + resetColor + error.message
+  );
+}
+
+function mergeLists(list1, list2, mergeFilePath) {
+  const firstList = list1;
+  const secondList = list2;
+  let newList = firstList.concat(secondList);
+  newList.sort();
+
+  try {
+    console.log(newList);
+    fs.writeFileSync(mergeFilePath, JSON.stringify(newList, null, 2), "utf8");
+
+    // fs.writeFile(mergeFilePath, , (a) => {
+    //   console.log("Added.", a);
+    // });
+  } catch (error) {
+    console.log(
+      redColor + "Error writing to JSON file:\n" + resetColor + error.message
+    );
+  }
+
+  //   return newList;
+
+  // process.exit(1);
+}
