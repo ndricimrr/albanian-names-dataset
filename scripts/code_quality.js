bd248f4c241a74997b50d8b0420608dc3d45e3c8
ndricimrr <ndricimrr@gmail.com>
2024-01-25 01:58:21 +0100

    Fix github actions

--- scripts/code_quality.js
+++ scripts/code_quality.js
@@ -1,4 +1,14 @@
-import { all_names } from "..";
+"use strict";
+
+const fs = require("fs");
+const jsonFilePath = "../all_names.json";
+
+try {
+  const all_names = require(`./${jsonFilePath}`);
+  hasDuplicateStrings(all_names);
+} catch (error) {
+  console.error("Error reading JSON file:", error.message);
+}
 
 function hasDuplicateStrings(array) {
   const seen = {};
@@ -6,12 +16,9 @@ function hasDuplicateStrings(array) {
   for (let i = 0; i < array.length; i++) {
     const currentString = array[i];
     if (seen[currentString]) {
-      return true;
+      console.error("Duplicate Name Found: " + currentString);
+      process.exit(1);
     }
     seen[currentString] = true;
   }
-
-  return false;
 }
-
-hasDuplicateStrings(all_names);
