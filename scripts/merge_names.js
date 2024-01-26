6435f5d92e099a1552df7585467beedd3cec2be9
ndricimrr <ndricimrr@gmail.com>
2024-01-26 01:32:52 +0100

    Fix merge script

--- scripts/merge_names.js
+++ scripts/merge_names.js
@@ -3,8 +3,8 @@
 const fs = require("fs");
 
 const all_names_path = "all_names.json";
-const male_names_path = "../female.json";
-const female_names_path = "../male.json";
+const female_names_path = "../female.json";
+const male_names_path = "../male.json";
 
 const redColor = "\x1b[31m";
 const resetColor = "\x1b[0m";
@@ -12,22 +12,38 @@ const greenColor = "\x1b[32m";
 const yellowColor = "\x1b[33m";
 
 try {
-  const male_names_list = require(`./${male_names_path}`);
-  const female_names_list = require(`./${female_names_path}`);
+  const males = require(`./${male_names_path}`);
+  const females = require(`./${female_names_path}`);
+  const mergedList = mergeLists(males, females, all_names_path);
 
-  mergeLists(male_names_list, female_names_list, all_names_path);
+  console.log(
+    `${greenColor}Finished merging  ${male_names_path} [${
+      males.length
+    }] + ${female_names_path}[${
+      females.length
+    }] into =>${resetColor} ${yellowColor}${all_names_path} [${
+      mergedList.length
+    }]=[${males.length + females.length}] ${resetColor}`
+  );
 } catch (error) {
   console.log(
     redColor + "Error reading JSON file:\n" + resetColor + error.message
   );
 }
 
+/**
+ * Function that takes two lists and merges them
+ * @param {Array} list1 first list
+ * @param {Array} list2 second list
+ * @param {string} mergeFilePath filepath of file to merge into
+ * @returns merged array sorted
+ */
 function mergeLists(list1, list2, mergeFilePath) {
   const firstList = list1;
   const secondList = list2;
   let newList = firstList.concat(secondList);
 
-  // Custom sorting function
+  // Custom sorting function for albanian alphabet
   const customSort = (a, b) => {
     const collator = new Intl.Collator("sq-AL", { sensitivity: "base" });
     return collator.compare(a, b);
@@ -36,19 +52,11 @@ function mergeLists(list1, list2, mergeFilePath) {
   newList.sort(customSort);
 
   try {
-    console.log(newList);
     fs.writeFileSync(mergeFilePath, JSON.stringify(newList, null, 2), "utf8");
-
-    // fs.writeFile(mergeFilePath, , (a) => {
-    //   console.log("Added.", a);
-    // });
+    return newList;
   } catch (error) {
     console.log(
       redColor + "Error writing to JSON file:\n" + resetColor + error.message
     );
   }
-
-  //   return newList;
-
-  // process.exit(1);
 }
